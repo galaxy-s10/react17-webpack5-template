8edac4a363816e5e6b4fad1ba26652bf36e1e100
shuisheng <2274751790@qq.com>
2022-12-05 09:30:23 +0800

    feat: 更新脚本

--- build.sh
+++ build.sh
@@ -6,13 +6,20 @@
 # Email: 2274751790@qq.com
 # FilePath: /github/react17-webpack5-template/build.sh
 # Github: https://github.com/galaxy-s10
-# LastEditTime: 2022-10-16 08:45:07
+# LastEditTime: 2022-12-05 09:29:25
 # LastEditors: shuisheng
 ###
 
 # 生成头部文件快捷键：ctrl+cmd+i
 
-# 该build.sh文件会在Jenkins构建完成后被执行
+# 静态部署的项目，一般流程是在jenkins里面执行build.sh进行构建，
+# 构建完成后会连接ssh，执行/node/sh/frontend.sh，frontend.sh会将构建的完成资源复制到/node/xxx。
+# 复制完成后，frontend.sh会执行清除buff/cache操作
+
+# node项目，一般流程是在jenkins里面执行build.sh进行构建，
+# 构建完成后会连接ssh，执行/node/sh/node.sh，node.sh会将构建的完成资源复制到/node/xxx，并且执行/node/xxx/pm2.sh。
+# 最后，node.sh会执行清除buff/cache操作
+
 # 注意:JOBNAME=$1,这个等号左右不能有空格！
 JOBNAME=$1      #约定$1为任务名
 ENV=$2          #约定$2为环境
@@ -24,33 +31,38 @@ PUBLICDIR=/node #约定公共目录为/node
 echo 删除node_modules:
 rm -rf node_modules
 
+echo 查看node版本:
+node -v
+
 echo 查看npm版本:
 npm -v
 
 echo 设置npm淘宝镜像:
-npm config set registry http://registry.npm.taobao.org/
+npm config set registry https://registry.npm.taobao.org/
 
 echo 查看当前npm镜像:
 npm get registry
 
-if ! type yarn >/dev/null 2>&1; then
-  echo 'yarn未安装,先全局安装yarn'
-  npm i yarn -g
+if ! type pnpm >/dev/null 2>&1; then
+  echo 'pnpm未安装,先全局安装pnpm'
+  npm i pnpm -g
 else
-  echo 'yarn已安装'
+  echo 'pnpm已安装'
 fi
 
-echo 查看yarn版本:
-yarn -v
+echo 查看pnpm版本:
+pnpm -v
 
-echo 设置yarn淘宝镜像:
-yarn config set registry https://registry.npm.taobao.org
+echo 设置pnpm淘宝镜像:
+pnpm config set registry https://registry.npm.taobao.org/
+pnpm config set @billd:registry http://registry.hsslive.cn/
 
-echo 查看当前yarn镜像:
-yarn config get registry
+echo 查看当前pnpm镜像:
+pnpm config get registry
+pnpm config get @billd:registry
 
 echo 开始安装依赖:
-yarn install
+pnpm install
 
 if [ $ENV = 'beta' ]; then
   echo 开始构建测试环境:
@@ -63,8 +75,3 @@ else
 fi
 
 npx cross-env REACT_APP_RELEASE_PROJECT_NAME=$JOBNAME REACT_APP_RELEASE_PROJECT_ENV=$ENV webpack --config ./script/config/webpack.common.ts --env production
-
-echo 清除buff/cache:
-
-sync
-echo 3 >/proc/sys/vm/drop_caches
